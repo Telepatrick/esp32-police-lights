ced1afe47352d8e06309b84d3e3e126735bb4796
eugene <eugenekobiliatsky@gmail.com>
2026-07-24 11:38:36 +0300

    Added two diodes-single pin variation

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,15 @@
 #include <Arduino.h>
 
-#define RED_LED_OUT 15
-#define WHITE_LED_OUT 16
-#define BLUE_LED_OUT 17
+#define LED_OUT 15
 
 void setup() {
-    pinMode(RED_LED_OUT, OUTPUT);
-    pinMode(WHITE_LED_OUT, OUTPUT);
-    pinMode(BLUE_LED_OUT, OUTPUT);
+    pinMode(LED_OUT, OUTPUT);
 }
 
 void loop() {
-  for(int i = 0; i< 3; i++) {
-    digitalWrite(BLUE_LED_OUT, HIGH);
-    digitalWrite(RED_LED_OUT, LOW);
-    delay(100);
-    digitalWrite(BLUE_LED_OUT, LOW);
-    digitalWrite(RED_LED_OUT, LOW);
-    delay(50);
-  }
-
-  for(int i = 0; i< 3; i++) {
-    digitalWrite(RED_LED_OUT, HIGH);
-    digitalWrite(BLUE_LED_OUT, LOW);
-    delay(100);
-    digitalWrite(BLUE_LED_OUT, LOW);
-    digitalWrite(RED_LED_OUT, LOW);
-    delay(50);
-  }
-
-  for(int i = 0; i< 3; i++) {
-    digitalWrite(WHITE_LED_OUT, HIGH);
-    delay(100);
-    digitalWrite(WHITE_LED_OUT, LOW);
+    digitalWrite(LED_OUT, LOW);
     delay(200);
-  }
+
+    digitalWrite(LED_OUT, HIGH);
+    delay(200);
 }
